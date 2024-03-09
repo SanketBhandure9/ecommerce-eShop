d8d70999cf6c05b4b9356eee6819a6acf0bec69d
Sanket Bhandure <sanketbhandure@outlook.com>
2024-03-09 14:01:40 +0530

    Responsiv and Sliding Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero/Hero";
 import NavBar from "./components/Navbar/NavBar";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   );
 };
