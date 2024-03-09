097bf70b64f5223b90728a7afe48a4dcb783cdf1
Sanket Bhandure <sanketbhandure@outlook.com>
2024-03-09 12:56:07 +0530

    Nav Dropdown Links

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,5 +1,5 @@
 import { IoMdSearch } from "react-icons/io";
-import { FaCartShopping } from "react-icons/fa6";
+import { FaCaretDown, FaCartShopping } from "react-icons/fa6";
 import DarkMode from "./DarkMode";
 
 const MenuLinks = [
@@ -22,6 +22,24 @@ const MenuLinks = [
     id: 4,
     name: "Blogs",
     link: "/#blog",
+  },
+];
+
+const DropdownLinks = [
+  {
+    id: 1,
+    name: "Trending Products",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Best Selling",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Top Rated",
+    link: "/#",
   },
 ];
 
@@ -51,6 +69,34 @@ const NavBar = () => {
                     </a>
                   </li>
                 ))}
+                {/* Dropdown */}
+                <li className="relative cursor-pointer group">
+                  <a
+                    href="#"
+                    className="flex items-center gap-[2px] font-semibold text-gray-500 hover:text-black dark:hover:text-white py-2"
+                  >
+                    {" "}
+                    Quick Links
+                    <span>
+                      <FaCaretDown className="group-hover:rotate-180 duration-300" />
+                    </span>
+                  </a>
+                  {/* Dropdown Links */}
+                  <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white shadow-md dark:bg-gray-900 p-2 dark:text-white">
+                    <ul className="space-y-2">
+                      {DropdownLinks.map((data) => (
+                        <li key={data.id}>
+                          <a
+                            className="text-gray-500 dark:hover:text-white duration-200 inline-block w-full p-2 hover:bg-primary/20 rounded-md font-semibold"
+                            href={data.link}
+                          >
+                            {data.name}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </li>
               </ul>
             </div>
           </div>
